3a62babd986eec0993ad68b394ead84cd6219533
Ridwan Adebayo <princekay145@gmail.com>
2021-10-21 23:07:02 +0300

    Day 3 completed

---
+++ Day3/Day3.py
@@ -0,0 +1,8 @@
+print('''
+Day 3, successfully completed.
+Worked with if, elif, and else statements
+For now, i'm working with my tutor of replit.com,
+so my codes are accessible only there for now..
+I also worked with logical operators "and", "or" and "not"...
+
+''')
